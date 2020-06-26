df6cfdc81e60f2cacf51255ff35906170f506544
Paige Andrews <pandrews25@yahoo.com>
2020-06-25 22:28:35 -0400

    drafting ideas for new project trying to find good api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+ 
+
+  useEffect(() => {
+    fetchItems();
+    }, []);
+
+
+  const fetchItems = async () => {
+    const data = await fetch("http://regions.ala.org.au/regions/regionList");
+    const items = await data.json();
+    console.log(items);
+    
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
